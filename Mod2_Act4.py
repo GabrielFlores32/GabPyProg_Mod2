bcc382af2c7f5dd345e9107cce6d134cce84e71d
Gabriel Flores <gabgabflores2@gmail.com>
2024-04-23 19:52:47 +0800

    1st commit

--- Mod2_Act4.py
+++ Mod2_Act4.py
@@ -2,6 +2,7 @@
 #all off the output must be in a file named "mylife.txt"
 
 def input_lines():
+    resume = True
     while resume:
         line = input("Enter line: ")
         space = line + ("\n")

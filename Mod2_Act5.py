8205d1dea6069a00141cad6f7164be068c43fd05
Gabriel Flores <gabgabflores2@gmail.com>
2024-04-23 20:37:41 +0800

    final na talaga

--- Mod2_Act5.py
+++ Mod2_Act5.py
@@ -2,12 +2,13 @@
 #find the odd and even numbers and put it to a seperate file.txt
 #and square the even numbers and cube the odd numbers
 
-with open("intergers.txt", "r") as integer_file, open("squared.txt", "w") as squared_file, open("cubed.txt", "w") as cubed_file:
+with open("integers.txt", "r") as integer_file, open("squared.txt", "w") as squared_file, open("cubed.txt", "w") as cubed_file:
     for number in integer_file:
-        if int(number) % 2 == 0:
-            square = int(number) ** 2
-            squared_file.write(square)
+        value = int(number)
+        if value % 2 == 0:
+            square = value ** 2
+            squared_file.write(str(square)+"\n")
         else:
-            cubed = int(number) ** 3
-            cubed_file.write(cubed)
+            cubed = value ** 3
+            cubed_file.write(str(cubed)+"\n")
             

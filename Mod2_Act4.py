cef6d42df04f0f4cc2dfb29af1a93a0ea3d1a0ef
Gabriel Flores <gabgabflores2@gmail.com>
2024-04-23 20:08:30 +0800

    final commit

--- Mod2_Act4.py
+++ Mod2_Act4.py
@@ -8,5 +8,16 @@ def input_lines():
         enter_line = line + ("\n")
         with open("mylife.txt", "a") as mylife_file:
             mylife_file.write(enter_line)
-            
+            while resume:
+                questions = input("Are there more lines y/n? ")
+                if questions.lower() == "y":
+                    break
+                elif questions.lower() == "n":
+                    print("say less")
+                    resume = False                   
+                else:
+                    print("Answer is invalid, use y/n only") 
+                    continue
+input_lines()              
+
         

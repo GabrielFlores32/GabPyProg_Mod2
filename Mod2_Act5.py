64060dd629e6f4b1f26bb98f5efa0fa4f3ac7dcc
Gabriel Flores <gabgabflores2@gmail.com>
2024-04-23 20:30:19 +0800

    final commit

--- Mod2_Act5.py
+++ Mod2_Act5.py
@@ -1,3 +1,13 @@
 #create a file.txt that 20 contains intergers 
 #find the odd and even numbers and put it to a seperate file.txt
 #and square the even numbers and cube the odd numbers
+
+with open("intergers.txt", "r") as integer_file, open("squared.txt", "w") as squared_file, open("cubed.txt", "w") as cubed_file:
+    for number in integer_file:
+        if int(number) % 2 == 0:
+            square = int(number) ** 2
+            squared_file.write(square)
+        else:
+            cubed = int(number) ** 3
+            cubed_file.write(cubed)
+            
